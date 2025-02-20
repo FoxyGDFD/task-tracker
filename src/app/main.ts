d397769ab1d82f6329150b76a6a2ca99617cf12b
PanfilovMS <panfilovms@git.corp.miigaik.ru>
2025-02-20 23:54:00 +0300

    feat: add json-server & api client

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
 import { router } from "./providers/router";
 import { Layout } from "./layouts";
 import './main.css'
 
-createApp(Layout).use(router).mount('#app')
+createApp(Layout).use(VueQueryPlugin).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './src/app'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-      '@entities': path.resolve(__dirname, './src/entities'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@app': path.resolve(__dirname, './src/app'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@widgets': path.resolve(__dirname, './src/widgets'),
+        '@entities': path.resolve(__dirname, './src/entities'),
+        '@features': path.resolve(__dirname, './src/features'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+      },
+    },
+    server: {
+      port: parseInt(env.PORT) ?? 5173,
+      proxy: {
+        [env.VITE_API_URL ?? '/api']: {
+          changeOrigin: true,
+          secure: false,
+          target: env.SERVER_URL ?? 'http://localhost:8000',
+        },
+      }
+    }
+  }
 })
